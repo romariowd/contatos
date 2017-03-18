f359fa70fc4ee43654a48714d02ff046eaaceeb4
Romário Silva dos Santos <romariowd@gmail.com>
2017-03-18 08:24:39 -0300

    Initial commit

---
+++ src/main/java/br/com/contatos/ContatosApplication.java
@@ -0,0 +1,23 @@
+package br.com.contatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+
+@Configuration
+@ComponentScan
+@EnableJpaRepositories
+@Import(RepositoryRestMvcConfiguration.class)
+@EnableAutoConfiguration
+public class ContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContatosApplication.class);
+
+	}
+
+}
